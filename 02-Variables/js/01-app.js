43dbed7958814c941c46b77d9e4af0e74732fc4d
valruizb05 <ing.valeriaruiz5@gmail.com>
2025-06-10 20:39:55 -0600

    update 02-Variables

--- 02-Variables/js/01-app.js
+++ 02-Variables/js/01-app.js
@@ -1,2 +1,15 @@
-const nombre = prompt("¿Cuál es tu nombre?");
-document.querySelector(".contenido").innerHTML = `${nombre} ESTA APRENDIENDO JAVASCRIPT!`;
+//Inicializar una variable con valor 
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+producto = 20;
+console.log(producto);
+
+//Inicializar múltiples variables
+var categoria = "Computadora",
+    marca = "LG",
+    calificacion = 5;
+
---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,13 @@
+let producto1 = "Tablet";
+
+producto = "Monitor";
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+let precio = 300;
+ 
+console.log(precio);
